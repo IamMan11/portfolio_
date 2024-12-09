bb03ef559de34cb443fd52fe36af3a18925e4b11
IamMan11 <epxt199@gmail.com>
2024-12-10 00:17:16 +0700

    asdw

--- app/page.tsx
+++ app/page.tsx
@@ -68,6 +68,7 @@ export default function Home() {
         <h2 className="text-2xl font-semibold mb-4">Contact</h2>
         <p>Email 66022826</p>
         <p>Phone: 0811329758</p>
+        <p>fackbook Puntakorn</p>
       </section>
     </main>
   </div>
